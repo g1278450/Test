e7504e14f9ab7fc5e5d1e0b3e4e42fcbb228c396
g1278450 <g1278450@gmail.com>
2021-01-10 15:12:15 +0800

    add a name Marry

---
+++ Dog.java
@@ -0,0 +1,18 @@
+
+public class Cat  {
+	private String name;
+	private Sting leg;
+	private String Marry;
+	
+	
+
+	
+	public Cat(String name) {
+		this.name = name;
+	}
+	
+	public void speak() {
+		System.out.println("This is Cat " + name + " speaking!");
+	}
+	
+}
